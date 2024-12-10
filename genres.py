35c50f604087ecc15f2fefb1997a73f5581669df
davey743 <dh43529p@pace.edu>
2024-12-10 23:47:58 +0000

    Update genres.py

--- genres.py
+++ genres.py
@@ -1,5 +1,5 @@
-Video Games = [Doom, Halo, Call of Duty, Mario, Counter Strike, Team Fortress, Overwatch, Grand Theft Auto, Fallout, Cry of Fear, Mortal Kombat, Bioshock, Garrys Mod, Outlast, Dead Space, Portal, 
-Half life, Hotline Miami, Tekken, Street Fighter, Sonic, Amnesia, Rainbow Six Seige, Silent Hill, Dark Souls, Five Nights at Freddys]
+video_games = ['Doom', 'Halo', 'Call of Duty', 'Mario', 'Counter Strike', 'Team Fortress', 'Overwatch', 'Grand Theft Auto', 'Fallout', 'Cry of Fear', 'Mortal Kombat', 'Bioshock', 'Garrys Mod', 'Outlast', 'Dead Space', 'Portal', 
+'Half life', 'Hotline Miami', 'Tekken', 'Street Fighter', 'Sonic', 'Amnesia', 'Rainbow Six Seige', 'Silent Hill', 'Dark Souls', 'Five Nights at Freddys']
 
-Music = [Deftones, Nirvana, The Smashing Pumpkins, Radiohead, Tyler the Creator, ASAP Rocky, The Garden, Pantera, My Bloody Valentine, Slipknot, TV Girl, Alice in Chains, Korn, Steve Lacy, System of Down,
-Playboi Carti, Title Fight, Flyleaf, Three Days Grace, Wiz Khalifa, Frank Ocean]
+music = ['Deftones', 'Nirvana', 'The Smashing Pumpkins', 'Radiohead', 'Tyler the Creator', 'ASAP Rocky', 'The Garden', 'Pantera', 'My Bloody Valentine', 'Slipknot', 'TV Girl', 'Alice in Chains', 'Korn', 'Steve Lacy', 'System of Down',
+'Playboi Carti', 'Title Fight', 'Flyleaf', 'Three Days Grace', 'Wiz Khalifa', 'Frank Ocean']
