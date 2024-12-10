3f0973e3122a52831ad7ec935f676b99d5a1bfa8
david <dh43529p@pace.edu>
2024-12-10 11:31:55 -0500

    hi

--- hangman.py
+++ hangman.py
@@ -1,4 +1,31 @@
-import random
+class genres:
+    def __init__(self, video_games, music):
+        self.video_games = video_games
+        self.music = music
+
+        
 
 def main():
-    pass
+    import random
+    import time
+
+    def rules():
+        print(f"Hangman is a wordguesser game, you have 7 tries to guess the word...")
+
+    def welcome():
+        print(f"Welcome to hangman!")
+        time.sleep(2)
+        print(f"""If you want to read the rules for the game type out "rules" """)
+        time.sleep(2)
+        print(f"""If you are ready to play type "play" """)
+        while True:
+            userinput = input("Response: ")
+            if userinput == "play":
+                break
+            if userinput == "rules":
+                rules()
+
+    welcome()
+
+
+main()
