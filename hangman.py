f756e2b94d444a0fcb687dcd6a0771f857ba391e
david <dh43529p@pace.edu>
2024-12-10 19:57:15 -0500

    update

--- hangman.py
+++ hangman.py
@@ -2,11 +2,29 @@ import random
 import time
 from genres import video_games, music
 
-class genres:
+class hangman:
     def __init__(self, video_games, music):
         self.video_games = video_games
-        self.music = music
+        self.music = music 
+        self.word_to_guess = None
+        self.max_guesses = 7
+        self.letterbank = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+    def select_word(self,category):
+        if category == "video_games":
+            self.word_to_guess = random.choice("video_games")
+        elif category == "music":
+            self.word_to_guess = random.choice("music")
+        else:
+            print("Invalid category")
     
+    def update_letterbank(self, guessed_letter):
+        if guessed_letter in self.letterbank:
+            self.wordbank = self.wordbank.replace(guessed_letter,"")
+
+    def display_letterbank(self):
+        spaced_letterbank = " ".join(self.letterbank)
+        print(f"Letters available: {spaced_letterbank}")
 
 def main():
     def rules():
@@ -18,13 +36,15 @@ def main():
         print(f"""If you want to read the rules for the game type out "rules" """)
         time.sleep(2)
         print(f"""If you are ready to play type "play" """)
+       
         while True:
             userinput = input("Response: ")
             if userinput == "play":
                 break
-            if userinput == "rules":
+            elif userinput == "rules":
                 rules()
-
+            else:
+                print("Invalid input. Please type 'play' to play or 'rules' to read the rules")
     welcome()
 
 
