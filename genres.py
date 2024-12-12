a37c5429e0701d42cf2070a53fc19087005b0439
christina-mc <cm05778p@pace.edu>
2024-12-11 21:43:38 -0500

    Update genres.py

--- genres.py
+++ genres.py
@@ -1,8 +1,8 @@
 video_games = ['Doom', 'Halo', 'Call of Duty', 'Mario', 'Counter Strike', 'Team Fortress', 'Overwatch', 'Grand Theft Auto', 'Fallout', 'Cry of Fear', 'Mortal Kombat', 'Bioshock', 'Garrys Mod', 'Outlast', 'Dead Space', 'Portal', 
 'Half life', 'Hotline Miami', 'Tekken', 'Street Fighter', 'Sonic', 'Amnesia', 'Rainbow Six Seige', 'Silent Hill', 'Dark Souls', 'Five Nights at Freddys']
 
-music = ['Deftones', 'Nirvana', 'The Smashing Pumpkins', 'Radiohead', 'Tyler the Creator', 'ASAP Rocky', 'The Garden', 'Pantera', 'My Bloody Valentine', 'Slipknot', 'TV Girl', 'Alice in Chains', 'Korn', 'Steve Lacy', 'System of Down',
-'Playboi Carti', 'Title Fight', 'Flyleaf', 'Three Days Grace', 'Wiz Khalifa', 'Frank Ocean']
+music_artists = ['Deftones', 'Nirvana', 'The Smashing Pumpkins', 'Radiohead', 'Tyler the Creator', 'ASAP Rocky', 'The Garden', 'Pantera', 'My Bloody Valentine', 'Slipknot', 'TV Girl', 'Alice in Chains', 'Korn', 'Steve Lacy', 'System of Down',
+'Playboi Carti', 'Title Fight', 'Flyleaf', 'Three Days Grace', 'Wiz Khalifa', 'Frank Ocean', 'Daniel Caesar', 'Brent Faiyaz', 'Childish Gambino', 'Bad Bunny', 'Kendrick Lamar', 'Bruno Mars', 'PARTYNEXTDOOR']
 
 movies = ['Star Wars', 'Jurassic Park', 'The Shawshank Redemption', 'Pulp Fiction', 'Toy Story', 'The Matrix', 'The Lord of the Rings', 'Pirates of the Carribean', 'Sherlock Holmes', 'The Silence of the Lambs', 'The Avengers', 'The Wizard of Oz', 'Casablanca', 'Gone with the Wind', 'Finding Nemo', 'The Lion King', 'Forrest Gump', 'Gladiator', 'The Godfather', 'The Sixth Sense', 'The Exorcist', 'The Shining', 'The Dark Knight Rises'] 
 
